2d02e6208023afd800c20380c5621d42b8de2619
miranbesen <mrnbsn33@gmail.com>
2024-09-29 23:49:05 +0300

    add footer and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import EmployeeList from './components/EmployeeList'
 
 function App() {
 
   return (
     <>
+     <HeaderComponent></HeaderComponent>
+     <p></p>
      <EmployeeList></EmployeeList>
+     <FooterComponent></FooterComponent>
     </>
   )
 }
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>Miran Besen Ürünüdür...</span>
+        </footer>
+    </div>
+  )
+}
+export default Footer
